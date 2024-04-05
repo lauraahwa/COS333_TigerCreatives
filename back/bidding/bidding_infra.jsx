2718084777682112bf1267156df27e2395b785e4
Laura Hwa <laurah@dynamic-oit-ip4-wifirestricted03-10-48-19-69.princeton.edu>
2024-04-04 20:36:15 -0400

    auth0, trying to stash

---
+++ back/bidding/bidding_infra.jsx
@@ -0,0 +1,87 @@
+import {
+    getDoc,
+    updateDoc,
+    doc,
+    Timestamp,
+    deleteField,
+  } from "firebase/firestore";
+  
+  import { db } from "./config";
+  import yaml from "js-yaml";
+  import { formatField } from "../utils/formatString";
+  
+  const parseField = (key) => {
+      const match = key.match(/item(\d+)_bid(\d+)/);
+      return {
+        item: Number(match[1]),
+        bid: Number(match[2]),
+      };
+    };
+    
+  export const unflattenItems = (doc, demo) => {
+    let items = {};
+    for (const [key, value] of Object.entries(doc.data())) {
+      const { item, bid } = parseField(key);
+  
+      if (!(item in items)) items[item] = { bids: {} };
+  
+      if (bid === 0) {
+        const { amount, endTime, ...itemData } = value;
+        // Spread operator on `items[item]` in case bid 0 wasn't the first to be read
+        items[item] = { ...items[item], ...itemData, startingPrice: amount, endTime: endTime.toDate() };
+        if (demo) {
+          const now = new Date();
+          items[item].endTime = new Date(
+            now.getFullYear(),
+            now.getMonth(),
+            now.getDate(),
+            now.getHours(),
+            items[item].endTime.getMinutes(),
+            items[item].endTime.getSeconds()
+          );
+        }
+      } else {
+        items[item].bids[bid] = value;
+      }
+    }
+    return Object.values(items);
+  };
+    
+  export const editItems = (id = undefined, update = false, reset = false) => {
+    fetch(import.meta.env.BASE_URL + "items.yml")
+      .then((response) => response.text())
+      .then((text) => yaml.load(text))
+      .then((items) => {
+        // If ID was provided, place that item in an array by itself
+        if (id !== undefined) items = [items.find((item) => item.id === id)];
+  
+        const docRef = doc(db, "auction", "items");
+        getDoc(docRef)
+          .then((doc) => {
+            console.debug("editItems() read from auction/items");
+            let fields = Object.keys(doc.data());
+            if (fields.length === 0)
+              fields = items.map((item) => formatField(item.id, 0));
+            const updates = {};
+            items.forEach((newItem) => {
+              // Convert ISO date into Firestore Timestamp
+              newItem.endTime = Timestamp.fromDate(new Date(newItem.endTime));
+              // Filter fields to the ones for the current newItem
+              fields
+                .filter((field) => parseField(field).item === newItem.id)
+                .forEach((field) => {
+                  if (update && parseField(field).bid === 0)
+                    updates[field] = newItem;
+                  if (reset && parseField(field).bid)
+                    updates[field] = deleteField();
+                });
+            });
+            return updates;
+          })
+          .then((updates) => {
+            updateDoc(docRef, updates);
+            console.debug("editItems() write to from auction/items");
+          });
+      });
+  };
+    
