69575460dce2c3c6ba9470a4d9f1da708eb34613
Laura Hwa <yh2673@princeton.edu>
2024-04-13 13:27:59 -0400

    conflict fixed

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,38 +4,14 @@ import {
   RouterProvider
 } from "react-router-dom"
 
-<<<<<<< HEAD
-import { Home, Read, Update, Delete, Login} from './pages'
-import { Navbar } from './components'
-=======
 import { Home,  } from './pages'
 import { Navbar, Footer } from './components'
->>>>>>> landing
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
-<<<<<<< HEAD
-  {
-    path: "/read",
-    element: <Read />
-  },
-  {
-    path: "/update",
-    element: <Update />
-  },
-  {
-    path: "/delete",
-    element: <Delete />
-  },
-  {
-    path: "/login",
-    element: <Login />
-  },
-=======
->>>>>>> landing
 ])
 
 function App() {
