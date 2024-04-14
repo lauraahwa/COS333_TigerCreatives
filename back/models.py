f482f88d7b33fa3da287259ae6a520091b7db0cd
Laura Hwa <laurah@dynamic-oit-ip4-wifirestricted03-10-48-19-69.princeton.edu>
2024-04-14 02:06:45 -0400

    bid tables

--- back/models.py
+++ back/models.py
@@ -1,6 +1,7 @@
 import os
 
 from extensions import db
+import datetime
 
 # _DATABASE_URL = os.environ['DATABASE_URL']
 # _DATABASE_URL = _DATABASE_URL.replace('postgres://', 'postgresql://')
@@ -64,3 +65,28 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String)
+
+# create an item to bid on
+class BidItem(db.Model):
+    __tablename__ = 'bid_item'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    start_time = db.Column(db.DateTime, default=datetime.utcnow)
+    end_time = db.Column(db.DateTime, nullable=False)
+
+    # relationships
+    bids = db.relationship('Bid', back_populates='item')
+
+# create a bid
+class Bid(db.Model):
+    __tablename__ = 'bids'
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    bid_time = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    biditem_id = db.Column(db.Integer, db.ForeignKey('biditem.id'))
+
+    # Relationships
+    user = db.relationship('User', back_populates='bids')
+    item = db.relationship('BidItem', back_populates='bids')
