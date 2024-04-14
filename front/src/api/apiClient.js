7aacfecef114df983e1f8579af55789bd1b8e229
lauraahwa <144400104+lauraahwa@users.noreply.github.com>
2024-04-14 14:16:44 -0400

    Update apiClient.js

--- front/src/api/apiClient.js
+++ front/src/api/apiClient.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
 
 const apiClient = axios.create({
-    baseURL: 'https://cos333-tigercreatives.onrender.com', // API base URL
+    baseURL: 'https://tigercreatives-back.onrender.com', // API base URL
     headers: {
         'Content-Type': 'application/json',
     }
