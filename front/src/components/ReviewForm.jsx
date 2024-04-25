9d5b23f41a854b7c6fa15020a06f10a27087b32b
ds7743 <102552583+ds7743@users.noreply.github.com>
2024-04-25 11:49:31 -0400

    Review form for sellers

---
+++ front/src/components/ReviewForm.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import StarRating from "./StarRating";
+
+const initialFormState = { rating: 0 };
+
+const ReviewForm = ({ setVisible, setReviews, reviews }) => {
+  const [formState, setFormState] = useState(initialFormState);
+  const [disabled, setDisabled] = useState(true);
+
+  const { rating } = formState;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setReviews([{ rating }, ...reviews]);
+    setVisible(false);
+    setFormState(initialFormState);
+  }
+
+  function handleRatingChange(rating) {
+    setFormState({ rating });
+    setDisabled(rating === 0);
+  }
+
+  return (
+    <div>
+      <StarRating onChange={handleRatingChange} value={rating} />
+      <form className="form" onSubmit={handleSubmit}>
+        <button className="btn" disabled={disabled} type="submit">
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ReviewForm;
