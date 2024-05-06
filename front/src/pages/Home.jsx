0d1c7f89e38e0842b8dda93e53031657380606a3
ds7743 <102552583+ds7743@users.noreply.github.com>
2024-05-06 00:55:51 -0400

    added services, discover, capped listings to 4

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components'
-import { viewListings, viewSortedAuctions } from '@/api/listingService'
-import { Grid, Button, ButtonContainer } from '@/components'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { viewListings, viewSortedAuctions } from "@/api/listingService";
+import { Grid, Button, ButtonContainer } from "@/components";
+import { Link } from 'react-router-dom';
 
-const Title = styled.div`
-
+const Title = styled.div
   display: flex;
   flex-direction: column;
   padding: 0 100px;
@@ -24,25 +24,25 @@ const Title = styled.div`
     margin-top: -0.5rem;
     font-size: clamp(1rem, 3vw, 2rem);
   }
-`
-const Container = styled.div`
+;
+
+const Container = styled.div
   display: flex;
   flex-direction: column;
   padding: 0 100px;
 
   justify-content: center;
   align-items: center;
+;
 
-`
-
-const Header = styled.h1`
+const Header = styled.h1
   font-size: 2rem;
   font-weight: 700;
   text-align: center;
   margin-bottom: 41px;
-`
+;
 
-const Banner = styled.div`
+const Banner = styled.div
   display: flex;
   width: 100%;
   flex-direction: column;
@@ -50,75 +50,132 @@ const Banner = styled.div`
   align-items: center;
   text-align: center;
   background-color: var(--container-color);
-
   padding: 78px 100px;
   margin-top: 130px;
 
   h1 {
     font-weight: 700;
-    font-size: 4rem;
+    font-size: clamp(1.5rem, 4vw, 8rem);
+    margin-bottom: 1rem;  /* Adds spacing between h1 and h2 */
+
+    i {
+      font-weight: 400;
+    }
   }
 
   h2 {
     font-weight: 500;
-    font-size: 2.5rem;
-    margin-bottom: 30px;
+    margin-top: -0.5rem;
+    margin-bottom: 1.5rem;  
+    font-size: clamp(1rem, 2vw, 2rem);
   }
-`
+  
+  .button-container {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+  }
+
+
+const WhiteDivider = styled.div
+  height: 150px;
+  background-color: white;
+;
 
 const Home = () => {
-  const [listingsData, setListingsData] = useState([])
-  const [auctionsData, setAuctionsData] = useState([])
+  const [listingsData, setListingsData] = useState([]);
+  const [auctionsData, setAuctionsData] = useState([]);
+  const [servicesData, setServicesData] = useState([]);
 
   useEffect(() => {
     const fetchListings = async () => {
       try {
-        const data = await viewListings('items');
+        const data = await viewListings("items");
         console.log("Data fetched", data);
-        setListingsData(data);
+        setListingsData(data.slice(0, 4));
       } catch (error) {
         console.error("Fetching listings error:", error);
       }
     };
-    // const fetchSortedAuctions = async () => {
-    //   try {
-    //     const data = await viewSortedAuctions();
-    //     console.log('auctions data fetched', data)
-    //     setAuctionsData(data)
-    //   } catch (error) {
-    //     console.error("Fetching auctions error", error)
-    //   }
-    // }
+    const fetchSortedAuctions = async () => {
+      try {
+        const data = await viewSortedAuctions();
+        console.log("auctions data fetched", data);
+        setAuctionsData(data.slice(0, 4));
+      } catch (error) {
+        console.error("Fetching auctions error", error);
+      }
+    };
+    const fetchServices = async () => {
+      try {
+        const data = await viewListings('services');
+        console.log("services data fetched", data);
+        setServicesData(data.slice(0,4));
+      } catch (error) {
+        console.error("Fetching services error:", error);
+      }
+    };
 
     fetchListings();
-    // fetchSortedAuctions()
+    fetchSortedAuctions();
+    fetchServices();
   }, []);
+
 
   return (
     <>
       <Title>
-        <h1>Tiger<i>Creatives</i></h1>
+        <h1>
+          Tiger<i>Creatives</i>
+        </h1>
         <h2>buy, sell, create.</h2>
       </Title>
       <Container>
-        {/* <Header>Discover</Header> */}
-        {/* <Grid isLanding={false} data={auctionsData} /> */}
+        <Header>Discover</Header>
+        <Grid isLanding={false} data={auctionsData} />
+
         <Header>Products</Header>
-        <Grid isLanding={true} data={listingsData}/>
+        <Grid isLanding={true} data={listingsData} />
         <ButtonContainer>
           <a href="/shop">
             <Button text="show more" />
           </a>
         </ButtonContainer>
 
+        <Header>Services</Header>
+        <Grid isLanding={true} data={servicesData} />
+        <ButtonContainer>
+          <a href="/services">
+            <Button text="show" />
+          </a>
+        </ButtonContainer>
       </Container>
       <Banner>
-          <h1>Are you a creative?</h1>
-          <h2>TigerCreatives is a platform for members of the Princeton community to list and purchase creative goods and services in a seamless fashion. From handmade jewelry to photography services, TigerCreatives aims to be Princetons go-to hub for artistry exchange. </h2>
-          <Button text="get started" />
+        <h1>Want a creative item or service? <br /></h1>
+        <h2>
+          Purchase something that your peers have thoughtfully <br />
+          offered to share with the Princeton community!<br />
+          We hope you'll find the sort of artistry that suits you. <br /> {" "}
+        </h2>
+        <Link to='/profile' style={{ textDecoration: 'none' }}>
+                <Button text="Login / Sign Up" style={{ width: '200px' }} onClick={() => loginWithRedirect()} />
+        </Link>
       </Banner>
+      <Banner>
+        <h1>Are you a creator? <br /></h1>
+        <h2>
+          Share your craft with members of the Princeton community!<br />
+          We're here to encourage your artistry, connect you to appreciators
+          of creativity, <br /> and be your go-to platform for celebrating
+          what you do best.<br /> {" "}
+        </h2>
+        <Link to='/profile' style={{ textDecoration: 'none', marginRight: '15px' }}>
+                <Button text="Login / Sign Up" style={{ width: '200px' }} onClick={() => loginWithRedirect()} /> 
+              </Link>
+      </Banner>
+      <WhiteDivider />
     </>
   );
-}
+};
 
 export default Home
